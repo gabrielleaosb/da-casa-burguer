e00144c560a3c93ab570d6c9999de79bda691277
Artista <gabriellsbagetti@gmail.com>
2025-04-28 15:59:12 -0300

    style admin login view

--- cardapio/urls.py
+++ cardapio/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('menu.urls')),  
+    path('', include('menu.urls')),
 ]
 
 if settings.DEBUG:
