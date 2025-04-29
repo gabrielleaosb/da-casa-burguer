f2616ff6e0cbea4c5b386e538815ba17d1df3746
Artista <gabriellsbagetti@gmail.com>
2025-04-29 17:25:57 -0300

    att

--- cardapio/settings.py
+++ cardapio/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '.ngrok.io']
+ALLOWED_HOSTS = ['*', '.ngrok.io', 'https://casaburguer.onrender.com/']
 
 
 # Application definition
